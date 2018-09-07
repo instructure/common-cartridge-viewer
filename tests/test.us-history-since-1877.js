02021da156e6abc07aa48f07952b9e17cddb4c97
Aaron Shafovaloff <ashafovaloff@instructure.com>
2018-09-07 13:53:07 -0600

    Wait longer to load test cartridge

--- tests/test.us-history-since-1877.js
+++ tests/test.us-history-since-1877.js
@@ -5,9 +5,11 @@ fixture`Dashboard`.page`http://localhost:5000/`;
 test("Organization item titles show for pages", async t => {
   await t
     .click(Selector("a").withText("US History Since 1877"))
-    .expect(Selector("h3").withText("Main Repository").exists)
+    .expect(Selector("h3").withText("Main Repository").exists, "foo", {
+      timeout: 5000
+    })
     .ok()
-    .expect(Selector("a").withText(`The First Measured Century`).exists)
+    .expect(Selector("a").withText(`The First Measured Century`).exists, "bar")
     .ok()
     .expect(
       Selector("a").withText(
