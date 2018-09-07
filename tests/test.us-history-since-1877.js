3947e86c7a008fe20a147e08c92cc1c09f586d0d
Aaron Shafovaloff <ashafovaloff@instructure.com>
2018-09-07 13:40:27 -0600

    Add test: module (organization) item titles show for pages

---
+++ tests/test.us-history-since-1877.js
@@ -0,0 +1,18 @@
+import { Selector } from "testcafe";
+
+fixture`Dashboard`.page`http://localhost:5000/`;
+
+test("Organization item titles show for pages", async t => {
+  await t
+    .click(Selector("a").withText("US History Since 1877"))
+    .expect(Selector("h3").withText("Main Repository").exists)
+    .ok()
+    .expect(Selector("a").withText(`The First Measured Century`).exists)
+    .ok()
+    .expect(
+      Selector("a").withText(
+        `The Women of Hull House: Harnessing Statistics for Progressive Reform`
+      ).exists
+    )
+    .ok();
+});
