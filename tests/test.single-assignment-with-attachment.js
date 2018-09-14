4d2e47a2413db93603b9388d58ef32c5146ab2ec
Aaron Shafovaloff <ashafovaloff@instructure.com>
2018-09-14 16:25:48 -0600

    Smarter showcasing of single resources

---
+++ tests/test.single-assignment-with-attachment.js
@@ -0,0 +1,27 @@
+import { Selector } from "testcafe";
+
+fixture`Single discussion cartridge (with attachment)`
+  .page`http://localhost:5000/?src=https%3A%2F%2Fs3.amazonaws.com%2Fpublic-imscc%2Fsingle-assignment.imscc#/`;
+
+test("Nav is displayed", async t => {
+  await t
+    .expect(Selector("h1").withText(`Assignment`).exists)
+    .ok()
+    .expect(Selector("nav").exists)
+    .ok()
+    .expect(Selector("header").exists)
+    .ok();
+});
+
+fixture`Single discussion cartridge (with attachment, compact)`
+  .page`http://localhost:5000/?compact&src=https%3A%2F%2Fs3.amazonaws.com%2Fpublic-imscc%2Fsingle-assignment.imscc#/`;
+
+test("Header is hidden", async t => {
+  await t
+    .expect(Selector("h1").withText(`Assignment`).exists)
+    .ok()
+    .expect(Selector("nav").exists)
+    .notOk()
+    .expect(Selector("header").exists)
+    .notOk();
+});
--- tests/test.single-page-no-attachment.js
+++ tests/test.single-page-no-attachment.js
@@ -1,20 +1,20 @@
 import { Selector } from "testcafe";
 
 fixture`Single page cartridge`
-  .page`http://localhost:5000/?src=https%3A%2F%2Fs3.amazonaws.com%2Fpublic-imscc%2Fsingle-page-cartridge.imscc#/`;
+  .page`http://localhost:5000/?src=https%3A%2F%2Fs3.amazonaws.com%2Fpublic-imscc%2Fsingle-page.imscc#/`;
 
-test("Nav is hidden", async t => {
+test("Nav is displayed", async t => {
   await t
     .expect(Selector("h1").withText(`Our Purpose`).exists)
     .ok()
     .expect(Selector("nav").exists)
-    .notOk()
+    .ok()
     .expect(Selector("header").exists)
     .ok();
 });
 
 fixture`Single page cartridge (compact)`
-  .page`http://localhost:5000/?compact&src=https%3A%2F%2Fs3.amazonaws.com%2Fpublic-imscc%2Fsingle-page-cartridge.imscc#/`;
+  .page`http://localhost:5000/?compact&src=https%3A%2F%2Fs3.amazonaws.com%2Fpublic-imscc%2Fsingle-page.imscc#/`;
 
 test("Header is hidden", async t => {
   await t
