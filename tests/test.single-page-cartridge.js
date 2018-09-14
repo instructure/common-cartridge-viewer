c8b2e2cb1889fd7805abf9cdccadb26f42dc87e4
Aaron Shafovaloff <ashafovaloff@instructure.com>
2018-09-14 13:34:09 -0600

    Add test for compact mode

--- tests/test.single-page-cartridge.js
+++ tests/test.single-page-cartridge.js
@@ -3,7 +3,20 @@ import { Selector } from "testcafe";
 fixture`Single page cartridge`
   .page`http://localhost:5000/?src=https%3A%2F%2Fs3.amazonaws.com%2Fpublic-imscc%2Fsingle-page-cartridge.imscc#/`;
 
-test("Nav and title is hidden", async t => {
+test("Nav is hidden", async t => {
+  await t
+    .expect(Selector("h1").withText(`Our Purpose`).exists)
+    .ok()
+    .expect(Selector("nav").exists)
+    .notOk()
+    .expect(Selector("header").exists)
+    .ok();
+});
+
+fixture`Single page cartridge (compact)`
+  .page`http://localhost:5000/?compact&src=https%3A%2F%2Fs3.amazonaws.com%2Fpublic-imscc%2Fsingle-page-cartridge.imscc#/`;
+
+test("Header is hidden", async t => {
   await t
     .expect(Selector("h1").withText(`Our Purpose`).exists)
     .ok()
