710b3ce9ba5a3d4e394cfe4b91938bb5116cca36
Caleb Guanzon <cguanzon@instructure.com>
2018-11-28 13:48:57 -0700

    Update test.module-items.js

--- tests/test.module-items.js
+++ tests/test.module-items.js
@@ -1,7 +1,7 @@
 import { Selector } from "testcafe";
 
 fixture`Course with 1 module (has all item types)`
-  .page`http://localhost:3000/?src=https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fcartridges-for-commons-preview%2FCOURSE-for-modules-testing.imscc#/`;
+  .page`http://localhost:3000/?src=https%3A%2F%2Fs3.amazonaws.com%2Fpublic-imscc%2FCOURSE-for-modules-testing.imscc#/`;
 
 test("Module Title is displayed", async t => {
   await t
@@ -11,8 +11,14 @@ test("Module Title is displayed", async t => {
 
 test("Assignment Items work", async t => {
   await t
-    .expect(Selector(".ExpandCollapseList-item:nth-of-type(1) svg[name='IconAssignment']").exists)
+    .expect(
+      Selector(".ExpandCollapseList-item:nth-of-type(1) svg[name='IconAssignment']").exists
+    )
     .ok()
-    .expect(Selector('.ExpandCollapseList-item:nth-of-type(1) a').withText('First Module Assignment 1').exists)
+    .expect(
+      Selector('.ExpandCollapseList-item:nth-of-type(1) a').withText(
+        'First Module Assignment 1'
+      ).exists
+    )
     .ok()
 });
