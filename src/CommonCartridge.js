356167a0f7e1687b819af3e357275556fbf59c5f
Mauricio Zaffari <mzaffari@instructure.com>
2018-11-29 17:03:19 -0200

    Use function from utils to find href attribute from a resource

--- src/CommonCartridge.js
+++ src/CommonCartridge.js
@@ -192,13 +192,10 @@ export default class CommonCartridge extends Component {
       const fileResources = resources
         .filter(is(resourceTypes.WEB_CONTENT))
         .filter(node => node.querySelector("file"))
-        .filter(
-          node =>
-            !node
-              .querySelector("file")
-              .getAttribute("href")
-              .startsWith("wiki_content/")
-        );
+        .filter(node => {
+          const href = getResourceHref(node) || "";
+          return !href.startsWith("wiki_content/");
+        });
       const assessmentResources = resources
         .filter(is(resourceTypes.ASSESSMENT_CONTENT))
         .filter(node => node.querySelector("file"));
