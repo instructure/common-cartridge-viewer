ab8888bdfc7eec07df7c2aa806399b73bcfa1b6c
Caleb Guanzon <cguanzon@instructure.com>
2018-11-29 10:32:41 -0700

    CM-602 add a test for each module item type

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ yarn start
 ```
 
 ## Run tests
+The tests try to visit the app in localhost:5000.
+Before running the tests, set `PORT=5000` as an
+environment variable to make them pass
 
 ```
 yarn run build
@@ -17,6 +20,6 @@ yarn test
 ## Usage example
 
 ```html
-<iframe sandbox="allow-scripts allow-same-origin" src="/?src=https://dl2ge9eav9ucb.cloudfront.net/facc06073092466
+<iframe sandbox="allow-scripts allow-same-origin" src="/?src=[COMMON-CARTRIDGE-VIEWER-DOMAIN]/facc06073092466
 38c298c6a1b01abcf.imscc"></iframe>
 ```
--- tests/test.module-items.js
+++ tests/test.module-items.js
@@ -15,10 +15,102 @@ test("Assignment Items work", async t => {
       Selector(".ExpandCollapseList-item:nth-of-type(1) svg[name='IconAssignment']").exists
     )
     .ok()
-    .expect(
+    .click(
       Selector('.ExpandCollapseList-item:nth-of-type(1) a').withText(
         'First Module Assignment 1'
-      ).exists
+      )
     )
+    .expect(Selector('h1').withText('First Module Assignment 1').exists)
+    .ok()
+});
+
+test("Quiz Items work", async t => {
+  const itemCss = '.ExpandCollapseList-item:nth-of-type(2)'
+  const title = `First Module Quiz 1`
+  await t
+    .expect(Selector(`${itemCss} svg[name='IconQuiz']`).exists)
+    .ok()
+    .expect(
+      Selector(
+        `${itemCss} div.ExpandCollapseList-item-details`
+      ).withText(`1 Questions`).exists)
+    .ok()
+    .click(Selector(`${itemCss} a`).withText(title))
+    .expect(Selector('h1').withText(title).exists)
+    .ok()
+});
+
+test("File Items work", async t => {
+  const itemCss = '.ExpandCollapseList-item:nth-of-type(3)'
+  const title = `341540_440x350.jpg`
+  await t
+    .expect(Selector(`${itemCss} svg[name='IconPaperclip']`).exists)
+    .ok()
+    .click(Selector(`${itemCss} a`).withText(title))
+    .expect(Selector('h1').withText(title).exists)
+    .ok()
+});
+
+test("File Items work", async t => {
+  const itemCss = '.ExpandCollapseList-item:nth-of-type(3)'
+  const title = `341540_440x350.jpg`
+  await t
+    .expect(Selector(`${itemCss} svg[name='IconPaperclip']`).exists)
+    .ok()
+    .click(Selector(`${itemCss} a`).withText(title))
+    .expect(Selector('h1').withText(title).exists)
+    .ok()
+});
+
+test("Wiki Page Items work", async t => {
+  const itemCss = '.ExpandCollapseList-item:nth-of-type(4)'
+  const title = `First Module Wiki Page 1`
+  await t
+    .expect(Selector(`${itemCss} svg[name='IconDocument']`).exists)
+    .ok()
+    .click(Selector(`${itemCss} a`).withText(title))
+    .expect(Selector('h1').withText(title).exists)
+    .ok()
+});
+
+test("Discussion Items work", async t => {
+  const itemCss = '.ExpandCollapseList-item:nth-of-type(5)'
+  const title = `First Module Discussion 1`
+  await t
+    .expect(Selector(`${itemCss} svg[name='IconDiscussion']`).exists)
+    .ok()
+    .click(Selector(`${itemCss} a`).withText(title))
+    .expect(Selector('h1').withText(title).exists)
+    .ok()
+});
+
+test("Text Headers work", async t => {
+  const itemCss = '.ExpandCollapseList-item:nth-of-type(6)'
+  const title = `First Module Text Header 1`
+  await t
+    .expect(Selector(`${itemCss} h3`).withText(title).exists)
+    .ok()
+});
+
+test("External URL's work", async t => {
+  const itemCss = '.ExpandCollapseList-item:nth-of-type(7)'
+  const title = `First Module External URL 1`
+  await t
+    .expect(Selector(`${itemCss} svg[name='IconExternalLink']`).exists)
+    .ok()
+    .click(Selector(`${itemCss} a`).withText(title))
+    .expect(Selector('h1').withText(title).exists)
+    .ok()
+});
+
+// fixing CM-599 will make this test pass. Skipping for now
+test.skip("External Tool URL's work", async t => {
+  const itemCss = '.ExpandCollapseList-item:nth-of-type(8)'
+  const title = `First Module AnalyTics Beta External Tool`
+  await t
+    .expect(Selector(`${itemCss} svg[name='IconExternalLink']`).exists)
+    .ok()
+    .click(Selector(`${itemCss} a`).withText(title))
+    .expect(Selector('h1').withText(title).exists)
     .ok()
 });
