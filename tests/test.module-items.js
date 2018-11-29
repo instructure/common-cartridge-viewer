07fc78fb8e6fba0d3c3bbd363758bf946e7f8ce7
Caleb Guanzon <calebguanzon@gmail.com>
2018-11-29 09:10:22 -0700

    begin splitting tests by functionality (#46)
    * begin splitting tests by functionality
    
    moves a test to new not-found test
    also introduces module-items tests
    
    future work will add more module-items tests
    
    * Update test.module-items.js
    
    * Update test.module-items.js
    
    * Update test.module-items.js

---
+++ tests/test.module-items.js
@@ -0,0 +1,24 @@
+import { Selector } from "testcafe";
+
+fixture`Course with 1 module (has all item types)`
+  .page`http://localhost:5000/?src=https%3A%2F%2Fs3.amazonaws.com%2Fpublic-imscc%2FCOURSE-for-modules-testing.imscc#/`;
+
+test("Module Title is displayed", async t => {
+  await t
+    .expect(Selector(".Module h3").withText(`First Module`).exists)
+    .ok()
+});
+
+test("Assignment Items work", async t => {
+  await t
+    .expect(
+      Selector(".ExpandCollapseList-item:nth-of-type(1) svg[name='IconAssignment']").exists
+    )
+    .ok()
+    .expect(
+      Selector('.ExpandCollapseList-item:nth-of-type(1) a').withText(
+        'First Module Assignment 1'
+      ).exists
+    )
+    .ok()
+});
---
+++ tests/test.not-found.js
@@ -0,0 +1,18 @@
+import { Selector } from "testcafe";
+
+fixture`Accessibility Workshop cartridge`.page`http://localhost:5000/`;
+
+test("Resource not found", async t => {
+  // first, wait for cartridge to load
+  await t
+    .click(Selector("a").withText("Ally: Accessibility Workshop"))
+    .expect(
+      Selector("h3").withText("Part 1: Overview: Accessibility and ALLY").exists
+    )
+    .ok("Header shows", { timeout: 20000 });
+
+  await t
+    .navigateTo("#/resources/notfound")
+    .expect(Selector("span").withText("Not found").exists)
+    .ok();
+});
