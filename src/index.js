27033efa1dc1dfc41d9e43f1bf89742c49aa3415
Aaron Shafovaloff <aaronshaf@gmail.com>
2018-12-04 13:53:57 -0700

    Beginning i18n support; upgrade create-react-app (#55)

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import "@instructure/ui-themes/lib/canvas";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import { I18nProvider } from "@lingui/react";
 
 const queryString = require("query-string-es5"); // has issue with module import
 const parsedQueryString = queryString.parse(window.location.search);
 
 ReactDOM.render(
-  <App
-    compact={typeof parsedQueryString.compact !== "undefined"}
-    manifest={parsedQueryString.manifest}
-    cartridge={parsedQueryString.src || parsedQueryString.cartridge}
-  />,
+  <I18nProvider language="en">
+    <App
+      compact={typeof parsedQueryString.compact !== "undefined"}
+      manifest={parsedQueryString.manifest}
+      cartridge={parsedQueryString.src || parsedQueryString.cartridge}
+    />
+  </I18nProvider>,
   document.getElementById("root")
 );
 
