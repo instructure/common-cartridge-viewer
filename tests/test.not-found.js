f0881a7d02919043a4ca0a50b67260052c51db2a
Aaron Shafovaloff <ashafovaloff@instructure.com>
2018-12-13 21:10:24 -0700

    Migrate some tests to local cartridges

--- tests/test.not-found.js
+++ tests/test.not-found.js
@@ -1,16 +1,11 @@
 import { Selector } from "testcafe";
 
-fixture`Accessibility Workshop cartridge`.page`http://localhost:5000/`;
+fixture`Accessibility Workshop cartridge`
+  .page`http://localhost:5000/?manifest=${encodeURIComponent(
+  "/test-cartridges/course-1/imsmanifest.xml"
+)}`;
 
 test("Resource not found", async t => {
-  // first, wait for cartridge to load
-  await t
-    .click(Selector("a").withText("Ally: Accessibility Workshop"))
-    .expect(
-      Selector("h3").withText("Part 1: Overview: Accessibility and ALLY").exists
-    )
-    .ok("Header shows", { timeout: 20000 });
-
   await t
     .navigateTo("#/resources/notfound")
     .expect(Selector("span").withText("Resource Unavailable").exists)
